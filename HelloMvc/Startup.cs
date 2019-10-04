f9986af9c1584001033158691bf7d27e5a3be26b
Kai Pridham <william@pridham.net.au>
2019-10-04 21:08:31 +1000

    feat: use path base ASPNETCORE_PATH_BASE if defined

--- HelloMvc/Startup.cs
+++ HelloMvc/Startup.cs
@@ -40,6 +40,11 @@ namespace HelloMvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            string pathBase = Environment.GetEnvironmentVariable("ASPNETCORE_PATH_BASE");
+            if (pathBase != null)
+                app.UsePathBase(pathBase);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
